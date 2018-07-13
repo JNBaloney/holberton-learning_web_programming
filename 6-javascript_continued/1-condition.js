e91fc4746ece5c5e4740e98ead4699e1777d5ee8
SaltyCereal <jeremiahnbaloney@gmail.com>
2018-07-13 15:10:42 -0700

    we

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,7 +1,6 @@
-
-let x = 7
-if (x > 5) {
+let myParameter = 7
+if (myParameter > 5) {
   console.log('The value is greater than 5');
 } else{
-  console.log('The value is equal to 5 or lower')
+  console.log('The value is equal to 5 or lower');
 }
