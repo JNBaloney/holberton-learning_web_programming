158ce96ebaa9dc6ea6e0ca873f91783e77c73f79
SaltyCereal <jeremiahnbaloney@gmail.com>
2018-07-13 15:33:39 -0700

    2

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,5 +1,5 @@
-let myParameter = 7
-if (myParameter > 5) {
+let my_parameter = process.argv[2]
+if (my_parameter> 5) {
   console.log('The value is greater than 5');
 } else{
   console.log('The value is equal to 5 or lower');
