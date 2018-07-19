a293a5d1417446cbe1fbafa79d4d881395f9bec6
SaltyCereal <jeremiahnbaloney@gmail.com>
2018-07-19 10:54:59 -0700

    Final_project

--- 6-javascript_continued/1-condition.js
+++ 6-javascript_continued/1-condition.js
@@ -1,7 +1,16 @@
-let my_parameter = process.argv[2]
-if (my_parameter> 5) {
-  console.log('The value is greater than 5');
-  //Print the my_parameter variable
-} else{
-  console.log('The value is equal to 5 or lower');
+let my_parameter = argv[2];
+if (my_parameter >= 95){
+  console.log(You received an A+);
+}if (my_parameter >= 90){
+  consol.log(You received an A);
+}if (my_parameter >= 85){
+  console.log(You received a B+);
+}if (my_parameter >= 80){
+  console.log(You received a B);
+}if (my_parameter >= 75){
+  console.log(You received a C+);
+}if (my_paramter >= 70{
+  console.log(You received a C);
+}if (my_paramter <=65){
+  console.log(You received a D);
 }
